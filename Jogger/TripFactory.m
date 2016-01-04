d2c3bf2019edab60dc419483fc5363d1c23e27f6
Thomas Sin <skyssw123@gmail.com>
2016-01-03 20:32:37 -0500

    wekrjwekr

--- Jogger/TripFactory.m
+++ Jogger/TripFactory.m
@@ -21,7 +21,21 @@
     else if(period == secondTrip)
         filePath = [NSString stringWithFormat:@"%@/%@", sharedInstance.dirPath, DEFAULT_SECOND_TRIP_FILENAME];
     else if(period == lastTrip)
+    {
         filePath = [NSString stringWithFormat:@"%@/%@", sharedInstance.dirPath, DEFAULT_LAST_TRIP_FILENAME];
+        if(![sharedInstance fileExists:filePath])
+        {
+            filePath = [NSString stringWithFormat:@"%@/%@", sharedInstance.dirPath, DEFAULT_SECOND_TRIP_FILENAME];
+            if(![sharedInstance fileExists:filePath])
+                filePath = [NSString stringWithFormat:@"%@/%@", sharedInstance.dirPath, DEFAULT_FIRST_TRIP_FILENAME];
+        }
+    }
+    
+    if(![sharedInstance fileExists:filePath])
+    {
+        //error banner goes in here (No Data)
+        return nil;
+    }
 
     DDFileReader * reader = [[DDFileReader alloc] initWithFilePath:filePath];
     NSString * line = nil;
@@ -39,54 +53,43 @@
             
         if([line isEqualToString:@"START OF TRIP\n"])
         {
-            selectedTrip ++;
             continue;
         }
-            
-        if(selectedTrip == period)
-        {
-            NSArray* components = [line componentsSeparatedByString:@","];
-            NSArray* timestampArray = [(NSString*)[components objectAtIndex:0] componentsSeparatedByString:@"="];
-            NSString* timestampString = [timestampArray objectAtIndex:1];
+        
+        NSArray* components = [line componentsSeparatedByString:@","];
+        NSArray* timestampArray = [(NSString*)[components objectAtIndex:0] componentsSeparatedByString:@"="];
+        NSString* timestampString = [timestampArray objectAtIndex:1];
                 
-            NSDateFormatter* formatter = [[NSDateFormatter alloc]init];
-            [formatter setDateFormat:@"yyyy-MM-dd HH:mm:ss z"];
-            NSDate* date = [formatter dateFromString:timestampString];
+        NSDateFormatter* formatter = [[NSDateFormatter alloc]init];
+        [formatter setDateFormat:@"yyyy-MM-dd HH:mm:ss z"];
+        NSDate* date = [formatter dateFromString:timestampString];
                 
-            NSArray* latitudeArray = [(NSString*)[components objectAtIndex:1] componentsSeparatedByString:@"="];
-            NSString* latitudeString = [latitudeArray objectAtIndex:1];
+        NSArray* latitudeArray = [(NSString*)[components objectAtIndex:1] componentsSeparatedByString:@"="];
+        NSString* latitudeString = [latitudeArray objectAtIndex:1];
                 
-            NSArray* longitudeArray = [(NSString*)[components objectAtIndex:2] componentsSeparatedByString:@"="];
-            NSString* longitudeString = [longitudeArray objectAtIndex:1];
+        NSArray* longitudeArray = [(NSString*)[components objectAtIndex:2] componentsSeparatedByString:@"="];
+        NSString* longitudeString = [longitudeArray objectAtIndex:1];
                 
-            CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake([latitudeString doubleValue], [longitudeString doubleValue]);
+        CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake([latitudeString doubleValue], [longitudeString doubleValue]);
                 
-            NSArray* speedArray = [(NSString*)[components objectAtIndex:3] componentsSeparatedByString:@"="];
-            NSString* speedString = [speedArray objectAtIndex:1];
+        NSArray* speedArray = [(NSString*)[components objectAtIndex:3] componentsSeparatedByString:@"="];
+        NSString* speedString = [speedArray objectAtIndex:1];
                 
-            NSArray* horizontalAccuracyArray = [(NSString*)[components objectAtIndex:4] componentsSeparatedByString:@"="];
-                                                                                                                                                                                                                                                                                                                                                                                          NSString* horizontalAccuracyString = [horizontalAccuracyArray objectAtIndex:1];
+        NSArray* horizontalAccuracyArray = [(NSString*)[components objectAtIndex:4] componentsSeparatedByString:@"="];
+        
+        NSString* horizontalAccuracyString = [horizontalAccuracyArray objectAtIndex:1];
                 
-            NSArray* verticalAccuracyArray = [(NSString*)[components objectAtIndex:5] componentsSeparatedByString:@"="];
-            NSString* verticalAccuracyString = [verticalAccuracyArray objectAtIndex:1];
+        NSArray* verticalAccuracyArray = [(NSString*)[components objectAtIndex:5] componentsSeparatedByString:@"="];
+        NSString* verticalAccuracyString = [verticalAccuracyArray objectAtIndex:1];
                 
-            NSArray* altitudeArray = [(NSString*)[components objectAtIndex:6] componentsSeparatedByString:@"="];
-            NSString* altitudeString = [(NSArray*)[[altitudeArray objectAtIndex:1] componentsSeparatedByString:@"\n"] objectAtIndex:0] ;
+        NSArray* altitudeArray = [(NSString*)[components objectAtIndex:6] componentsSeparatedByString:@"="];
+        NSString* altitudeString = [(NSArray*)[[altitudeArray objectAtIndex:1] componentsSeparatedByString:@"\n"] objectAtIndex:0] ;
                 
-            CLLocation* location = [[CLLocation alloc]initWithCoordinate:coordinate altitude:[altitudeString doubleValue] horizontalAccuracy:[horizontalAccuracyString doubleValue] verticalAccuracy:[verticalAccuracyString doubleValue] course:0 speed:[speedString doubleValue] timestamp:date];
-            [returnArray addObject:location];
-        }
+        CLLocation* location = [[CLLocation alloc]initWithCoordinate:coordinate altitude:[altitudeString doubleValue] horizontalAccuracy:[horizontalAccuracyString doubleValue] verticalAccuracy:[verticalAccuracyString doubleValue] course:0 speed:[speedString doubleValue] timestamp:date];
+        [returnArray addObject:location];
     }
     
-    if(period == lastTrip)
-    {
-        if(selectedTrip == firstTrip)
-            return [self produceTripWithLogs:firstTrip];
-        else if(selectedTrip == secondTrip)
-            return [self produceTripWithLogs:secondTrip];
-    }
-    
-    else if(returnArray == nil)
+    if(returnArray == nil)
         return nil;
     
     trip = [[Trip alloc]initWithLocations:returnArray];
